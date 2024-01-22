f113f0723750b75216f747f0095e25f7b09b871b
Hukkatir <dudinassonya@gmail.com>
2024-01-22 15:09:24 +0300

    маппинг

---
+++ документирование api/BackendApi/BackendApi/Controllers/TextTypeController.cs
@@ -0,0 +1,109 @@
+﻿using BackendApi.Contracts.TextType;
+using BackendApi.Models;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextTypeController : ControllerBase
+    {
+        public dbapiContext Context { get; }
+
+        public TextTypeController(dbapiContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        /// Получение всех записей текста 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAll() //получение всех записей 
+        {
+            List<TextType> textTypes = Context.TextTypes.ToList();
+            return Ok(textTypes);
+        }
+
+        /// <summary>
+        /// ПОлучение записи текста по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")] // получение одной записи по id
+        public IActionResult GetById(int id)
+        {
+            TextType textTypes = Context.TextTypes.Where(x => x.TextTypeId == id).FirstOrDefault();
+            if (textTypes == null)
+            {
+                return BadRequest("Не найдено");
+            }
+            return Ok(textTypes);
+        }
+
+
+        /// <summary>
+        /// Слоздание записи текста 
+        /// </summary>
+        /// <remarks>
+        /// Пример запроса:
+        /// 
+        ///     {   "blockId": 1,
+        ///         "content": "Этот текст написан курсивом",
+        ///         "bold": false,
+        ///         "italic": true,
+        ///         "strikethrough": false,
+        ///         "underline": false,
+        ///         "code": false,
+        ///         "color": "green"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="textTypes"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Add(CreateTextTypeRequest request) //добавление записей 
+        {
+            var textTyprDto = request.Adapt<TextType>();
+            Context.TextTypes.Add(textTyprDto);
+            Context.SaveChanges();
+            return Ok(textTyprDto);
+        }
+
+        /// <summary>
+        /// Обновление данных текста
+        /// </summary>
+        /// <param name="textTypes"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Update(CreateTextTypeRequest request) //Обновление данных (Изменение существующей записи)
+        {
+            var textTyprDto = request.Adapt<TextType>();
+            Context.TextTypes.Update(textTyprDto);
+            Context.SaveChanges();
+            return Ok(textTyprDto);
+        }
+
+        /// <summary>
+        /// Удаление записи типа текста
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            TextType? textTypes = Context.TextTypes.Where(x => x.TextTypeId == id).FirstOrDefault();
+            if (textTypes == null)
+            {
+                return BadRequest("Не найдено");
+
+            }
+            Context.TextTypes.Remove(textTypes);
+            Context.SaveChanges();
+            return Ok(textTypes);
+        }
+    }
+}
---
+++ документирование api/BackendApi/BackendApi/Controllers/VideoController.cs
@@ -0,0 +1,108 @@
+﻿using BackendApi.Contracts.Category;
+using BackendApi.Contracts.Video;
+using BackendApi.Models;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        public dbapiContext Context { get; }
+
+        public VideoController(dbapiContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        /// Получение всех видео записей 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAll() //получение всех записей 
+        {
+            List<Video> videos = Context.Videos.ToList();
+            return Ok(videos);
+        }
+
+        /// <summary>
+        /// Получени езаписи видео по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")] // получение одной записи по id
+        public IActionResult GetById(int id)
+        {
+            Video videos = Context.Videos.Where(x => x.VideoId == id).FirstOrDefault();
+            if (videos == null)
+            {
+                return BadRequest("Не найдено");
+            }
+            return Ok(videos);
+        }
+
+
+        /// <summary>
+        /// Создание записи видео 
+        /// </summary>
+        /// <remarks>
+        /// Пример запроса: 
+        /// 
+        ///     {
+        ///         "blockId": 1,
+        ///         "videoUrl": "https://youtu.be/cxr3b_Gzmls?si=9HN4fiOKZ0IVqRi_",
+        ///          "note": "Почему «Оппенгеймер», кажется, главный фильм года"
+        ///     }
+        ///     
+        /// </remarks>
+        /// <param name="videos"></param>
+        /// <returns></returns>
+        [HttpPost]
+
+        public IActionResult Add(CreateVideoRequest request) //добавление записей 
+        {
+
+            var videoDto = request.Adapt<Video>();
+            Context.Videos.Add(videoDto);
+            Context.SaveChanges();
+            return Ok(videoDto);
+        }
+
+        /// <summary>
+        /// Обновление данных 
+        /// </summary>
+        /// <param name="videos"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Update(CreateVideoRequest request) //Обновление данных (Изменение существующей записи)
+        {
+            var videoDto = request.Adapt<Video>();
+            Context.Videos.Update(videoDto);
+            Context.SaveChanges();
+            return Ok(videoDto);
+        }
+
+        /// <summary>
+        /// Удаление записи видео по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Video? videos = Context.Videos.Where(x => x.VideoId == id).FirstOrDefault();
+            if (videos == null)
+            {
+                return BadRequest("Не найдено");
+
+            }
+            Context.Videos.Remove(videos);
+            Context.SaveChanges();
+            return Ok(videos);
+        }
+    }
+}
