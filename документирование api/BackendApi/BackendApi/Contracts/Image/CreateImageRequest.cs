104e7a44f5be0d701d6cbc361b7d18915b3f59e5
Hukkatir <dudinassonya@gmail.com>
2024-01-23 23:33:34 +0300

    d

--- документирование api/BackendApi/BackendApi/Contracts/Image/CreateImageRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackendApi.Contracts.Image
-{
-    public class CreateImageRequest
-    {
-        public int BlockId { get; set; }
-        public string ImageUrl { get; set; } = null!;
-        public string? Note { get; set; }
-    }
-}
--- документирование api/BackendApi/BackendApi/Contracts/Video/CreateVideoRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackendApi.Contracts.Video
-{
-    public class CreateVideoRequest
-    {
-        public int? BlockId { get; set; }
-        public string VideoUrl { get; set; } = null!;
-        public string? Note { get; set; }
-    }
-}
