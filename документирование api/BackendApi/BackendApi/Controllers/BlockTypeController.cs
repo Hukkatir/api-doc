6e0e6ee6cf09b5de25e13c4694a948de0dff56d8
Hukkatir <dudinassonya@gmail.com>
2024-01-23 23:34:07 +0300

    up

---
+++ документирование api/BackendApi/BackendApi/Controllers/BlockTypeController.cs
@@ -0,0 +1,105 @@
+﻿using BackendApi.Contracts.BlockType;
+using BackendApi.Contracts.User;
+using BackendApi.Models;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlockTypeController : ControllerBase
+    {
+        public dbapiContext Context { get; }
+
+        public BlockTypeController(dbapiContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        /// Получение всех записей
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAll() //получение всех записей 
+        {
+            List<BlockType> blockTypes = Context.BlockTypes.ToList();
+            return Ok(blockTypes);
+        }
+        /// <summary>
+        /// Получение записи по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")] // получение одной записи по id
+        public IActionResult GetById(int id)
+        {
+            BlockType blockTypes = Context.BlockTypes.Where(x => x.BlockTypeId == id).FirstOrDefault();
+            if (blockTypes == null)
+            {
+                return BadRequest("Не найдено");
+            }
+            return Ok(blockTypes);
+        }
+
+        /// <summary>
+        /// Создание новой записи типа блока
+        /// </summary>
+        /// <remarks>
+        /// Пример запроса:
+        /// 
+        ///     {
+        ///         "blockId": 4,
+        ///         "textTypeId": 4,
+        ///         "videoId": 3,
+        ///         "imageId": 4
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="blockTypes"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Add(CreateBlockTypeRequest request) //добавление записей 
+        {
+            var blockTyprDto = request.Adapt<BlockType>();
+            Context.BlockTypes.Add(blockTyprDto);
+            Context.SaveChanges();
+            return Ok(blockTyprDto);
+        }
+
+        /// <summary>
+        /// Обновление дынных записи 
+        /// </summary>
+        /// <param name="blockTypes"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Update(CreateBlockTypeRequest request) //Обновление данных (Изменение существующей записи)
+        {
+            var blockTyprDto = request.Adapt<BlockType>();
+            Context.BlockTypes.Update(blockTyprDto);
+            Context.SaveChanges();
+            return Ok(blockTyprDto);
+        }
+
+        /// <summary>
+        /// Удаление записи типа блока по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            BlockType? blockTypes = Context.BlockTypes.Where(x => x.BlockTypeId == id).FirstOrDefault();
+            if (blockTypes == null)
+            {
+                return BadRequest("Не найдено");
+
+            }
+            Context.BlockTypes.Remove(blockTypes);
+            Context.SaveChanges();
+            return Ok(blockTypes);
+        }
+    }
+}
---
+++ документирование api/BackendApi/BackendApi/Controllers/CategoryController.cs
@@ -0,0 +1,107 @@
+﻿using BackendApi.Contracts.Category;
+using BackendApi.Contracts.User;
+using BackendApi.Models;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        public dbapiContext Context { get; }
+
+        public CategoryController(dbapiContext context)
+        {
+            Context = context;
+        }
+        /// <summary>
+        /// Получение всех записей категорий
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAll() //получение всех записей 
+        {
+            List<Category> categories = Context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        /// <summary>
+        /// Получение записи категориии по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")] // получение одной записи по id
+        public IActionResult GetById(int id)
+        {
+            Category categories = Context.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            if (categories == null)
+            {
+                return BadRequest("Не найдено");
+            }
+            return Ok(categories);
+        }
+
+
+        /// <summary>
+        /// Создание новой записи категории
+        /// </summary>
+        /// <remarks>
+        /// Пример запроса:
+        /// 
+        ///     {
+        ///         "categoryName": "Новость",
+        ///         "descrip": "Новости из мира кино",
+        ///         "createdBy": 1,
+        ///         "createdAt": "2024-01-21T18:57:41.342Z"
+        ///     }
+        ///     
+        /// </remarks>
+        /// <param name="categories"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Add(CreateCategoryRequest request) //добавление записей 
+        {
+            var categoryDto = request.Adapt<Category>();
+            Context.Categories.Add(categoryDto);
+            Context.SaveChanges();
+            return Ok(categoryDto);
+        }
+
+        /// <summary>
+        /// Обновление данных категорий
+        /// </summary>
+        /// <param name="categories"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Update(CreateCategoryRequest request) //Обновление данных (Изменение существующей записи)
+        {
+            var categoryDto = request.Adapt<Category>();
+            Context.Categories.Update(categoryDto);
+            Context.SaveChanges();
+            return Ok(categoryDto);
+        }
+
+        /// <summary>
+        /// Удаление записи категории по id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Category? categories = Context.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            if (categories == null)
+            {
+                return BadRequest("Не найдено");
+
+            }
+            Context.Categories.Remove(categories);
+            Context.SaveChanges();
+            return Ok(categories);
+        }
+    }
+}
